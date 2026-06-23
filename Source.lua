53111fd7ec4b0f4e7298c3ab30c91861f4ea6698
haru7932 <s7023042@gmail.com>
2026-06-23 06:46:10 -0300

    Update image asset ID for Arrow in Source.lua

--- Source.lua
+++ Source.lua
@@ -1698,7 +1698,7 @@ function redzLib:MakeWindow(Configs)
         Size = UDim2.new(0, 16, 0, 16),
         Position = UDim2.new(0, -5, 0.5, 0),
         AnchorPoint = Vector2.new(1, 0.5),
-        Image = "rbxassetid://113192319418076",
+        Image = "rbxassetid://72510282085222",
         BackgroundTransparency = 1,
         Rotation = 180,
         ImageColor3 = Theme["Color Stroke"]
